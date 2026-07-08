48dee967dfe23cc2bd7b6c837e28e81c8e43885c
brandoncrespo161 <BRANDONCRESPO161@GMAIL.COM>
2026-07-08 01:20:56 +0000

    metadata

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,25 @@ const cormorant = Cormorant_Garamond({ subsets: ['latin'], weight: ['300', '400'
 const montserrat = Montserrat({ subsets: ['latin'], weight: ['300', '400', '600'], variable: '--font-montserrat' });
 
 export const metadata = {
-  title: 'Boda Brandon & Mery Laura',
+  title: 'Boda de Brandon & Mery Laura',
+  description: 'Con inmensa alegría queremos invitarte a celebrar el día de nuestra boda este 18 de julio.',
+  openGraph: {
+    title: 'Nuestra Boda | Brandon & Mery Laura',
+    description: '¡Toca aquí para abrir tu invitación y descubrir todos los detalles!',
+    url: 'https://brandonmeryboda.vercel.app',
+    siteName: 'Boda Brandon & Mery Laura',
+    images: [
+      {
+        // Usaremos tu sello como imagen de portada para WhatsApp
+        url: '/sello2.png', 
+        width: 800,
+        height: 600,
+        alt: 'Sello de Lacre B&M',
+      },
+    ],
+    locale: 'es_BO',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
